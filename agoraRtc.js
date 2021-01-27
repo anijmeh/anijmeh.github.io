c197f66374acf461583cc995ca7b9f166de99315
anijmeh <73448705+anijmeh@users.noreply.github.com>
2021-01-27 17:10:53 -0500

    Add files via upload

--- agoraRtc.js
+++ agoraRtc.js
@@ -1,10 +1,37 @@
+let n = 0;
 function addVideoStream(streamId) {
-    let remoteContainer = document.getElementById("remoteStream");
+    let which = "remoteStream1";
+    if (n==1)
+    {
+        which = "remoteStream2";
+    }
+    else if (n==2)
+    {
+        which = "remoteStream3";
+    }
+    else if (n==3)
+    {
+        which = "remoteStream4";
+    }
+
+    console.log(which);
+
+    let remoteContainer = document.getElementById(which);
     let newDiv = document.createElement("div");
     newDiv.id = streamId;
     newDiv.style.transform = "rotateY(180deg)";
-    newDiv.style.height = "250px";
+    newDiv.style.height = "100%";
+    newDiv.style.alignItems = "flex-start";
     remoteContainer.appendChild(newDiv);
+    n++;
+}
+
+function addName(name) {
+    let list = document.getElementById("names");
+    let newName = document.createTextNode(name);
+    list.appendChild(newName);
+    var linebreak = document.createElement('br');
+    list.appendChild(linebreak);
 }
 
 document.getElementById("join-button").onclick = function () {
@@ -21,14 +48,16 @@ document.getElementById("join-button").onclick = function () {
         channel,
         username,
         () => {
-            var clientStream = AgoraRTC.createStream({video: true, audio: true, });
+            var clientStream = AgoraRTC.createStream({video: true, audio: true});
             
             clientStream.init(function (){
                 clientStream.play("selfStream");
                 console.log("client streaming");
                 client.publish(clientStream);
+                addName(username);
             })
-        }
+        },
+        () => console.log(err)
     )
 
     client.on("stream-added", function (evt){
@@ -40,6 +69,7 @@ document.getElementById("join-button").onclick = function () {
         console.log("Subscribed Stream");
         let stream = evt.stream;
         addVideoStream(stream.getId());  
+        addName(stream.getId());
         stream.play(stream.getId());
     })
 }
